7266d8016062104f3293cb0954835d8b67733235
Abeeb-Raji <67653660+AbuHubayb@users.noreply.github.com>
2023-01-14 21:42:23 +0100

    Update

--- cypress/e2e/Extranet/registration.cy.js
+++ cypress/e2e/Extranet/registration.cy.js
@@ -1,63 +1,74 @@
-describe('sign-in', () => {
-  beforeEach('visit-bluepilo', ()=>{
+describe('SIGN-IN', () => {
+  beforeEach('visit-bluepilo', () => {
     cy.visit('/')
   })
+
   it('landing - page', () => {
     cy.get('.d-none')
-    cy.get(':nth-child(1) > .h4').should('contain','Bluepilo Inventory System')
+    cy.get(':nth-child(1) > .h4').should('contain', 'Bluepilo Inventory System')
   })
-  it.only('invalid credentials', () => {
+  it('invalid email', () => {
     cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
     cy.get(':nth-child(2) > .form-control').type('password')
     cy.get('#submitButton').click()
     cy.get('small').should('contain', 'Invalid email')
   })
-  it('invalid email', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
-    cy.get(':nth-child(2) > .form-control').type('password')
-    cy.get('#submitButton').click()
-  })
   it('invalid password', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
-    cy.get(':nth-child(2) > .form-control').type('password')
+    cy.get(':nth-child(1) > .form-control').type('almahbuub@gmail.com')
+    cy.get(':nth-child(2) > .form-control').type('passwordd')
     cy.get('#submitButton').click()
+    cy.get('small').should('contain', 'Invalid Password')
   })
   it('empty email', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
     cy.get(':nth-child(2) > .form-control').type('password')
     cy.get('#submitButton').click()
+    cy.url().should('eq', 'https://dev.bluepilo.com/login')
   })
   it('empty password', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
     cy.get(':nth-child(2) > .form-control').type('password')
     cy.get('#submitButton').click()
+    cy.url().should('eq', 'https://dev.bluepilo.com/login')
   })
   it('empty credentials', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
-    cy.get(':nth-child(2) > .form-control').type('password')
     cy.get('#submitButton').click()
+    cy.url().should('eq', 'https://dev.bluepilo.com/login')
   })
-  it('credential mismatch', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
-    cy.get(':nth-child(2) > .form-control').type('password')
-    cy.get('#submitButton').click()
+  it('Navigate to PasswordResetPage', () => {
+    cy.get('.small').click()
+    cy.url().should('eq', 'https://dev.bluepilo.com/forgot-password')
   })
-  it('valid credentials', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > .form-control').type('email@eamil.com')
-    cy.get(':nth-child(2) > .form-control').type('password')
-    cy.get('#submitButton').click()
+  it('Navigate To RegistartionPage', () => {
+    cy.get(':nth-child(4) > a').click()
+    cy.url().should('eq', 'https://dev.bluepilo.com/register-business')
+  })
+})
+
+
+
+describe('FORGOT PASSWORD', () => {
+  beforeEach('visit-bluepilo-forgot-password', () => {
+    cy.visit('/forgot-password')
+  })
+  it('valid email', () => {
+    cy.get('.form-control').type('almahbuub@gmail.com')
+    cy.get('.form-control').click()
+  })
+  it('invalid email', () => {
+    cy.get('.form-control').type('miskeenboy@gmail.com{enter}')
+  })
+  it('back to login', () => {
+    cy.get('.small').click()
+    cy.url().should('eq', 'https://dev.bluepilo.com/login')
+  })
+})
+
+
+
+describe.only('REGISTRATION', () => {
+  beforeEach('visit-reg_page', () => {
+    cy.visit('/register-business')
   })
 
-  it('passes', () => {
-    cy.visit('/')
-    cy.get(':nth-child(4) > a').click()
+  it('Navigate To RegistartionPage', () => {
   })
-  
 })
